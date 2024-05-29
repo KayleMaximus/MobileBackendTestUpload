4695eb5800fd93297addc295e40759677513892d
Zeniittt <tinnqd1702@gmail.com>
2024-05-29 14:55:40 +0700

    add API update isLove

--- src/app/controllers/ListenHistoryController.js
+++ src/app/controllers/ListenHistoryController.js
@@ -1,4 +1,4 @@
-const { db, storage } = require("../../config/db/firebase");
+const { db } = require("../../config/db/firebase");
 const ListenHistory = require("../models/ListenHistory");
 
 class ListenHistoryController {
@@ -30,20 +30,54 @@ class ListenHistoryController {
     console.log(userID);
     try {
       // Truy vấn dữ liệu người dùng từ Firestore
-      const userRef = db.collection("listenHistory").where("userID", "==", userID);
+      const userRef = db
+        .collection("listenHistory")
+        .where("userID", "==", userID);
       const myUser = await userRef.get();
-  
+
       console.log(myUser);
-  
+
       let userDataList = [];
       myUser.forEach((doc) => {
         userDataList.push(doc.data());
       }); // Lấy tất cả dữ liệu người dùng từ Firestore
-  
+
       res.status(200).json(userDataList);
     } catch (error) {
       console.error("Error fetching user:", error);
       res.status(500).json({ error: "Internal server error" });
+    }
+  }
+
+  async update(req, res) {
+    const { userID, songID, isLove } = req.body;
+
+    try {
+      // Tìm tài liệu có trường userID phù hợp
+      const historyRef = db
+        .collection("listenHistory")
+        .where("userID", "==", userID)
+        .where("songID", "==", songID)
+        .limit(1);
+      const history = await historyRef.get();
+
+      if (history.empty) {
+        // Nếu không tìm thấy tài liệu nào phù hợp
+        res.status(404).send("No matching documents found");
+      } else {
+        // Nếu tìm thấy tài liệu phù hợp
+        const doc = history.docs[0];
+
+        // Sử dụng arrayUnion để thêm phần tử vào cuối mảng 'content'
+        await doc.ref.update({
+          isLove: isLove
+        });
+
+        res.status(200).send("Updated Field Value isLove successfully");
+      }
+    } catch (error) {
+      console.error("Error updating user: ", error);
+      res.status(500).send("Internal Server Error");
     }
   }
 }
--- src/routes/listenHistory.route.js
+++ src/routes/listenHistory.route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const listenHistoryController = require('../app/controllers/ListenHistoryController');
 const listenHistory = require('../app/middlewares/listenHistory');
 
+router.patch('/updateIsLove', listenHistoryController.update);
 router.get('/:userID', listenHistoryController.getHistoryByUserID);
 router.get('/',  listenHistoryController.index);
 router.post('/', listenHistory);
