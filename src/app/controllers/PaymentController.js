d03286c14353142380d623c43ffca55e1fb88b42
Zeniittt <tinnqd1702@gmail.com>
2024-06-10 22:41:01 +0700

    trả lại sự trong sạch cho payment

--- src/app/controllers/PaymentController.js
+++ src/app/controllers/PaymentController.js
@@ -10,7 +10,7 @@ class PaymentController {
     const { amountFromCLient } = req.body;
     //Create a PaymentIntent with the order amount and currency
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: 23000,
+      amount: amountFromCLient,
       currency: "usd",
       // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
       automatic_payment_methods: {
