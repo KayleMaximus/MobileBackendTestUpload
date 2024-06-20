50a27180747473292d9237381c02056b1f55d4b1
Zeniittt <tinnqd1702@gmail.com>
2024-06-20 13:08:12 +0700

    côm mít me sít

--- src/index.js
+++ src/index.js
@@ -1,86 +1,90 @@
 const { db, storage } = require("./config/db/firebase.js");
 const { v4: uuidv4 } = require("uuid");
-const path = require("path")
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 const multer = require("multer");
 const { FieldValue } = require("firebase-admin/firestore");
-const cors = require('cors');
-require('dotenv').config();
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 const io = new Server(server);
 
-const route = require('./routes')
+const route = require("./routes");
 
-
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
-app.use(morgan('combined')); //HTTP logger
- //Template Engine
-app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources', 'views'));
+app.use(morgan("combined")); //HTTP logger
+//Template Engine
+app.engine("hbs", handlebars.engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources", "views"));
 
 const port = process.env.PORT || 8383;
 route(app);
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log("User connected");
 
-  socket.on('create-room', data => {
-      socket.join(data.roomID);
-      io.to(data.roomID).emit('on-create-room', data.roomID); 
-      io.to(data.roomID).emit('on-join-room', data.userName);
-      console.log(`User joined room ${data.roomID}`);
+  //hadnle create room
+  socket.on("create-room", (data) => {
+    socket.join(data.roomID);
+    io.to(data.roomID).emit("on-create-room", data.roomID);
+    io.to(data.roomID).emit("on-join-room", data.userName);
+    console.log(`User joined room ${data.roomID}`);
   });
 
-  socket.on('join-room', data=>{
+  //handle join room
+  socket.on("join-room", (data) => {
     socket.join(data.roomID);
-    io.to(data.roomID).emit('on-get-room-info', data.userName);
-    console.log(`User ${data.userName}  asking room ${data.roomID} and waiting for response`);
-  })
+    io.to(data.roomID).emit("on-get-room-info", data.userName);
+    console.log(
+      `User ${data.userName}  asking room ${data.roomID} and waiting for response`
+    );
+  });
 
-  socket.on('respone-room-info', data=>{
+  socket.on("respone-room-info", (data) => {
     socket.join(data.roomID);
-    io.to(data.roomID).emit('on-respone-room-info', data.roomInfo)
+    io.to(data.roomID).emit("on-respone-room-info", data.roomInfo);
     console.log(`response from ${data.roomID} : ${data.roomInfo}`);
-  })
+  });
 
-  socket.on('user-join-room', data=>{
+  socket.on("user-join-room", (data) => {
     socket.join(data.roomID);
-    io.to(data.roomID).emit('on-user-join-room', data.userName);
+    io.to(data.roomID).emit("on-user-join-room", data.userName);
     console.log(`User ${data.userName} joined room ${data.roomID}`);
-  })
+  });
 
-  // socket.on('sendMessage', (room, message) => {
-  //     io.to(room).emit('message', message);
-  // });
+  // handle message
+  socket.on("user-message", (data) => {
+    io.to(data.roomID).emit("on-user-message", data.message);
+  });
 
-  // socket.on('on-chat', data => {
-  //     io.emit('user-chat', data)
-  // })
+  // handle play song
+  socket.on("on-add-song", (data) => {
+    io.to(data.roomID).emit("on-song-added", data.song);
+  });
 
-  socket.on('on-add-song', data => {
-    io.to(data.room).emit('on-song-added', data.song);
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 });
 
-  socket.on('disconnect', () => {
-      console.log('User disconnected');
-  });
-})
-
 server.listen(port, () => console.log(`Server has started on ${port}`));
-
